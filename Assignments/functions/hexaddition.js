005c478337784b3e9486cad37276ad9bca0e1763
thee freelancer <ronaldssebalamu2@gmail.com>
2023-04-07 06:51:25 +0300

    Add Hexaddition function

---
+++ Assignments/functions/hexaddition.js
@@ -0,0 +1,4 @@
+function hexAddition(a, b) {
+    return (a + b).toString(16);
+}
+console.log(hexAddition(19, 23));
---
+++ Assignments/functions/hexaddition.ts
@@ -0,0 +1,4 @@
+function hexAddition(a: number, b: number): string{
+        return (a + b).toString(16);
+}
+console.log(hexAddition(19, 23));
